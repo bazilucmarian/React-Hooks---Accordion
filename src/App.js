42c29ee4a6fdc850d951e1980f0997d4d6685bf1
bazilucmarian <48621132+bazilucmarian@users.noreply.github.com>
2020-08-03 06:59:09 -0700

    create dropdown

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 
-const items=[
+const items = [
 
     {
-        title:'What is React?',
+        title: 'What is React?',
         content: 'React is a front end js framework'
     },
     {
-        title:'Why use React?',
+        title: 'Why use React?',
         content: 'React is a favourite js library among engineers'
     },
     {
-        title:'Why do you useReact?',
+        title: 'Why do you useReact?',
         content: 'For creating components to show content for user '
     }
 ]
 
-const App=()=> {
+const options = [
+    {
+        label: 'the color Red',
+        value: 'red'
+
+    },
+    {
+        label: 'the color green',
+        value: 'green'
+
+
+    },
+    {
+        label: 'the color blue',
+        value: 'blue'
+    }
+
+]
+
+
+const App = () => {
+    const [selected,setSelected]=useState(options[0])
     return (
         <div>
-            <br/>
-        {/* <Accordion items={items}/> */}
-        <Search/>
+            <br />
+            {/* <Accordion items={items}/> */}
+            {/* <Search/> */}
+            <Dropdown options={options} 
+            onSelectedChange={setSelected}
+            selected={selected}/>
 
         </div>
     )
